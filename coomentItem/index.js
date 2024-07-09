67db2aca7d06c36dbf417a0186e666fe1c76895a
DivyaRamachandrapu <169077489+DivyaRamachandrapu@users.noreply.github.com>
2024-07-09 21:23:01 +0530

    Update index.js

--- coomentItem/index.js
+++ coomentItem/index.js
@@ -1,9 +1,10 @@
 import {formatDistanceToNow} from 'date-fns'
+
 import './index.css'
 
 const CommentItem = props => {
   const {commentDetails} = props
-  const {id, name, comment, date, initialClassName, isLiked} = commentDetails
+  const {id, name, comment, isLiked, initialClassName, date} = commentDetails
   const initial = name ? name[0].toUpperCase() : ''
   const likeTextClassName = isLiked ? 'button active' : 'button'
   const likeImageUrl = isLiked
